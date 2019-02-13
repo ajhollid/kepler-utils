42fc5de8c0caeaa50ebeb84328785e9d512c7048
Alex <ajhollid@gmail.com>
2019-02-13 12:58:28 -0800

    Add solar system to exports, rename planets to solar system

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const OrbitalUtils = require( './orbital_utils' );
 const JulianUtils = require( './julian_utils' );
+const SolarSystem = require( './solar_system' );
 
 module.exports = {
   OrbitalUtils,
   JulianUtils,
+  SolarSystem,
 };
 
