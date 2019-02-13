e6871302d52830c1829ed4f2cb1224e26ad43e76
Alex <ajhollid@gmail.com>
2019-02-13 11:08:27 -0800

    Complete calcMeanAnom function

--- orbital_utils.js
+++ orbital_utils.js
@@ -1,8 +1,15 @@
 const orbitalApp = ( function () {
   const SCALE = 80;
+
+  const calcMeanAnom = ( L, lPeri ) => {
+    let M = L - lPeri;
+    while ( M < 0 ) {
+      M += 360;
+    }
+    return M;
+  };
 }() );
 
 module.exports = {
-
 };
 
